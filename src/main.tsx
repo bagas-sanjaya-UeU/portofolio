a21719ffa3a0c8c0d1ac0714f107438e3d5c3263
bagas-sanjaya-UeU <charderrasanjaya@student.esaunggul.ac.id>
2026-05-02 22:11:58 +0700

    refactor: clean up imports in App and main files

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-import { TerminalContextProvider } from "react-terminal"; // Tambahkan ini
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import './index.css'
+import { TerminalContextProvider } from "react-terminal"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <TerminalContextProvider> {/* Bungkus App di sini */}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <TerminalContextProvider>
       <App />
     </TerminalContextProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
